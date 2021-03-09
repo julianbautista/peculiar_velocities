80d873b9e8562cde43a2dfacf8683862c7549bfc
julianbautista <julian.bautista@port.ac.uk>
2021-03-09 16:14:09 +0100

    using jit parallel now

--- pv_covariance.py
+++ pv_covariance.py
@@ -2,10 +2,14 @@ import numpy as np
 import astropy
 from astropy.table import Table
 import pylab as plt
+
 import scipy.integrate
 import scipy.special
+import scipy.stats
+
 from cosmo import CosmoSimple
-from numba import jit
+from numba import jit, prange
+
 import time
 import multiprocessing 
 
@@ -21,6 +25,7 @@ def separation(r_0, r_1, cos_alpha):
     return np.sqrt(r_0**2 + r_1**2 - 2*r_0*r_1*cos_alpha)
 
 def j0(x):
+    ''' This doesn't work with numba '''
     return scipy.special.spherical_jn(0, x)
 
 @jit(nopython=True)
@@ -28,6 +33,7 @@ def j0_alt(x):
    return np.sin(x)/x
     
 def j2(x):
+    ''' This doesn't work with numba '''
     return scipy.special.spherical_jn(2, x)
 
 @jit(nopython=True)
@@ -65,56 +71,47 @@ def test_window():
     plt.ylabel(r'$W_{i,j}(k)$')
 
 @jit(nopython=True)
-def covariance(k, pk, win):
-    return np.trapz(pk * win, x=k)
+def get_covariance(ra_0, dec_0, r_comov_0, ra_1, dec_1, r_comov_1, k, pk):
+    ''' Get cosmological covariance for a given pair of galaxies 
+        and a given power spectrum (k, pk) in units of h/Mpc and (Mpc/h)^3
+    '''
+    cos_alpha = angle_between(ra_0, dec_0, ra_1, dec_1)
+    win = window(k, r_comov_0, r_comov_1, cos_alpha)
+    cova = np.trapz(pk * win, x=k)
+    return cova
 
-
-
-@jit(nopython=True)
-def get_covariance_matrix(ra, dec, r_comov, k, pk, h):
-    
-    variance = covariance(k, pk, 1/3)
-
-    nh = len(ra)
+@jit(nopython=True, parallel=True)
+def build_covariance_matrix(ra, dec, r_comov, k, pk):
+    ''' Builds a 2d array with the theoretical covariance matrix 
+        based on the positions of galaxies (ra, dec, r_comov) 
+        and a given power spectrum (k, pk)
+    '''
+    nh = ra.size
     cov_matrix = np.zeros((nh, nh))
-    for i in range(nh):
-        cov_matrix[i, i] = variance
+    for i in prange(nh):
+        ra_0 = ra[i]
+        dec_0 = dec[i]
+        r_comov_0 = r_comov[i]
         for j in range(i+1, nh):
-            cos_alpha = angle_between(ra[i], dec[i], ra[j], dec[j])
-            win = window(k, r_comov[i], r_comov[j], cos_alpha)
-            cov = covariance(k, pk, win)
+            ra_1 = ra[j]
+            dec_1 = dec[j]
+            r_comov_1 = r_comov[j]
+            cov = get_covariance(ra_0, dec_0, r_comov_0, ra_1, dec_1, r_comov_1, k, pk)
             cov_matrix[i, j] = cov
             cov_matrix[j, i] = cov
 
-    cov_matrix *= (h*100)**2/(2*np.pi**2) 
-    return cov_matrix  
+    #-- For diagonal, window = 1/3
+    var = np.trapz(pk/3, x=k)
+    np.fill_diagonal(cov_matrix, var)
 
-@jit(nopython=True)
-def get_covariance_parallel(index):
-    i, j = index 
-    cos_alpha = angle_between(ra[i], dec[i], ra[j], dec[j])
-    win = window(k, r_comov[i], r_comov[j], cos_alpha)
-    cova = covariance(k, pk, win)
-    return cova
+    #-- Pre-factor H0^2/(2pi^2)
+    cov_matrix *= (100)**2/(2*np.pi**2) 
 
-def correlation(cov):
+    return cov_matrix
+
+def corr_from_cov(cov):
     diag = np.diag(cov)
     return cov/np.sqrt(np.outer(diag, diag))
-
-def reduce_resolution(k, pk, kmin=None, kmax=None, nk=None, linear=False):
-
-    if kmin is None:
-        kmin = k.min()
-    if kmax is None:
-        kmax = k.max()
-    if nk is None:
-        nk = k.size
-    if linear:
-        knew = np.linspace(kmin, kmax, nk)
-    else:
-        knew = 10**np.linspace(np.log10(kmin), np.log10(kmax), nk)
-    pknew = np.interp(knew, k, pk)
-    return knew, pknew
 
 def read_power_spectrum(non_linear=False, redshift_space=False, 
                         kmin=None, kmax=None, nk=None):
@@ -145,8 +142,26 @@ def read_power_spectrum(non_linear=False, redshift_space=False,
 
     return k, pk
 
-def check_integrals(k, pk, scales=[1., 3., 10., 30., 100.]):
-        
+def reduce_resolution(k, pk, kmin=None, kmax=None, nk=None, linear=False):
+
+    if kmin is None:
+        kmin = k.min()
+    if kmax is None:
+        kmax = k.max()
+    if nk is None:
+        nk = k.size
+    if linear:
+        knew = np.linspace(kmin, kmax, nk)
+    else:
+        knew = 10**np.linspace(np.log10(kmin), np.log10(kmax), nk)
+    pknew = np.interp(knew, k, pk)
+    return knew, pknew
+
+def check_integrals():
+
+    k, pk = read_power_spectrum(nk=100000)
+    scales = [1., 3., 10., 30., 100.]
+
     plt.figure()
     for i_scale, scale in enumerate(scales):
         win_radial = window(k, 200., 200+scale, 1.)
@@ -200,49 +215,39 @@ def read_halos(cosmo=None, redshift_space=False, nhalos=None):
     
     return ra, dec, r_comov, vel
 
-def build_covariance_matrix(ra, dec, r_comov, vel, k, pk, pool=None):
-
-    nh = ra.size
-    indices = [(i, j) for i in range(nh) for j in range(i+1, nh)]
-
-    if not pool is None:
-        results = pool.map(get_covariance_parallel, indices)
-
-    cov_matrix = np.zeros((nh, nh))
-    for ind, cov in zip(indices, results):
-        i, j = ind
-        cov_matrix[i, j] = cov
-        cov_matrix[j, i] = cov
-    np.fill_diagonal(cov_matrix, covariance(k, pk, 1/3))
-
-    #-- Pre-factor H0^2/(2pi^2)
-    cov_matrix *= (100)**2/(2*np.pi**2) 
-
-    return cov_matrix
-
 @jit(nopython=True)
-def log_like(x, cova):
+def log_likelihood(x, cova):
+    ''' Computes log of the likelihood from 
+        a vector x and a covariance cova
+    '''
     nx = x.size
     eigvals = np.linalg.eigvalsh(cova)
     inv_matrix = np.linalg.inv(cova)
     chi2 = x.T @ inv_matrix @ x
-    log_likes = -0.5*(nx*np.log(2*np.pi) 
+    log_like = -0.5*(nx*np.log(2*np.pi) 
                       + np.sum(np.log(eigvals))
                       + chi2)
-    return log_likes
+    return log_like
 
-def get_log_likes(f_value):
-
-    this_log_likes = np.zeros(sig_values.size)
-    for j in range(sig_values.size):
-        #-- Total matrix
+@jit(nopython=True, parallel=True)
+def get_log_likes(vel, cov_cosmo, f_values, sig_values):
+    ''' Computes 2d array containing the log likelihoood
+        as a function f and sig
+    '''
+    diag_cosmo = np.diag(cov_cosmo)
+    log_likes = np.zeros((f_values.size, sig_values.size))
+    for i in prange(f_values.size):
+        f_value = f_values[i]
         cov_matrix = cov_cosmo*f_value**2 
-        #-- Add intrinsic dispersion to diagonal 
-        d = np.einsum('ii->i', cov_matrix) 
-        d += sig_values[j]**2
-        #-- Compute likelihood
-        this_log_likes[j] = log_like(vel, cov_matrix)
-    return this_log_likes
+        for j in range(sig_values.size):
+            sig_value = sig_values[j]
+            #-- Total matrix
+            #-- Add intrinsic dispersion to diagonal
+            diag_total = diag_cosmo*f_value**2 + sig_value**2
+            np.fill_diagonal(cov_matrix, diag_total)
+            #-- Compute likelihood
+            log_likes[i, j] = log_likelihood(vel, cov_matrix)
+    return log_likes
 
 def read_likelihood(fin):
     f_values, sig_values, like = np.loadtxt(fin, unpack=1)
@@ -251,119 +256,79 @@ def read_likelihood(fin):
     like = np.reshape(like, (f_values.size, sig_values.size))
     return f_values, sig_values, like
 
-#ef main():
+def plot_likelihood(fin):
 
-cosmo = CosmoSimple(omega_de=0.32, h=0.7, mass_neutrinos=0.)
+    f_values, sig_values, likelihood = read_likelihood(fin)
 
-nhalos = 1000
-kmax = 1.
-nk = 512
-non_linear = True
-redshift_space = False
-parallel = True
-one_dim = False
-sig_v = 100. # km/s
+    #-- Cumulative distribution at 1, 2, 3 sigma for one degree of freedom
+    cdf = scipy.stats.chi2.cdf([9, 4, 1], 1)
+    #-- corresponding chi2 values for 2 degrees of freedom
+    chi2_values = scipy.stats.chi2.ppf(cdf, 2)
+    #-- corresponding likelihood values
+    like_contours = np.exp(-0.5*chi2_values)
 
-print('Number of selected halos:', nhalos)
-print('kmax:', kmax)
-print('nk:', nk)
-print('Non-linear:', non_linear)
-print('Redshift-space:', redshift_space)
-print('Parallel:', parallel)
-print('sig_v:', sig_v)
-
-k, pk = read_power_spectrum(non_linear=non_linear, 
-                            redshift_space=redshift_space, 
-                            kmin=None, kmax=kmax, nk=nk)
-
-ra, dec, r_comov, vel = read_halos(cosmo=cosmo, 
-    redshift_space=redshift_space, 
-    nhalos=nhalos)
-
-#-- Compute covariance matrix
-t0 = time.time()
-if parallel:
-    context = multiprocessing.get_context('fork')
-    print("Number of cpus available: ", multiprocessing.cpu_count())
-    with context.Pool(processes=multiprocessing.cpu_count()) as pool:
-        cov_cosmo = build_covariance_matrix(ra, dec, r_comov, vel, k, pk, pool=pool)    
-else:
-    cov_cosmo = build_covariance_matrix(ra, dec, r_comov, vel, k, pk)
-t1 = time.time()
-print(f'Time elapsed calculating cov matrix {(t1-t0)/60:.2f} minutes')
+    plt.figure()
+    plt.contour(f_values, sig_values, likelihood.T, levels=like_contours,
+                colors='k', linestyles='--')
+    plt.pcolormesh(f_values, sig_values, likelihood.T, shading='nearest', cmap='gray_r')
+    plt.colorbar()
+    plt.xlabel(r'Growth-rate $f$')
+    plt.ylabel(r'$\sigma_v$ [km/s]')
 
 
-print('Cov:')
-print(cov_cosmo[:5, :5])
+def main():
 
-#-- Scan over f values
-f_expected = 0.523
+    cosmo = CosmoSimple(omega_de=0.32, h=0.7, mass_neutrinos=0.)
 
-n_values = 40
-f_values = np.linspace(0.2, 0.5, n_values)
-sig_values = np.linspace(100, 300., n_values)
-if one_dim:
-    log_likes = np.zeros( n_values )
+    nhalos = 1000
+    kmax = 1.
+    nk = 512
+    non_linear = True
+    redshift_space = False
+    sig_v = 100. # km/s
+    output_likelihood = 'like_v2.txt'
+
+    print('Number of selected halos:', nhalos)
+    print('kmax:', kmax)
+    print('nk:', nk)
+    print('Non-linear:', non_linear)
+    print('Redshift-space:', redshift_space)
+    print('sig_v:', sig_v)
+
+    k, pk = read_power_spectrum(non_linear=non_linear, 
+                                redshift_space=redshift_space, 
+                                kmin=None, kmax=kmax, nk=nk)
+
+    ra, dec, r_comov, vel = read_halos(cosmo=cosmo, 
+        redshift_space=redshift_space, 
+        nhalos=nhalos)
+
+    #-- Compute covariance matrix
+    t0 = time.time()
+    cov_cosmo = build_covariance_matrix(ra, dec, r_comov, k, pk)    
+    t1 = time.time()
+    print(f'Time elapsed calculating cov matrix {(t1-t0)/60:.2f} minutes')
+
+
+    print('Cov:')
+    print(cov_cosmo[:5, :5])
+
+    #-- Scan over f values
+    f_expected = 0.523
+    n_values = 50
+    f_values = np.linspace(0.2, 0.5, n_values)
+    sig_values = np.linspace(100, 300., n_values)
+    log_likes = get_log_likes(vel, cov_cosmo, f_values, sig_values)
+    t2 = time.time()
+    print(f'Time elapsing with likelihood grid: {(t2-t1)/60:.2f} minutes or {(t2-t1)/n_values**2:.2f} sec per entry')
+
+    likelihood = np.exp(log_likes-log_likes.max())
+
+    fout = open(output_likelihood, 'w')
     for i in range(n_values):
-        print(i, n_values)
-        #-- Total matrix
-        cov_matrix = cov_cosmo*f_values[i]**2
-        #-- Add intrinsic dispersion to diagonal 
-        d = np.einsum('ii->i', cov_matrix) 
-        d += sig_v**2
-        #-- Compute likelihood
-        log_likes[i] = log_like(vel, cov_matrix)
-else:    
-    log_likes = np.zeros( (n_values, n_values) )
-    
-    if 1==1:
-        with context.Pool(processes=multiprocessing.cpu_count()) as pool:
-            results = pool.map(get_log_likes, f_values)
-        for i in range(n_values):
-            for j in range(n_values):
-                log_likes[i, j] = results[i][j]
-    else:
-        for i in range(n_values):
-            print(i, n_values)
-            log_likes[i] = get_log_likes(f_values[i])
-
-
-t2 = time.time()
-print(f'Time elapsing with likelihood grid: {(t2-t1)/60:.2f} minutes or {(t2-t1)/n_values**2:.2f} sec per entry')
-
-likelihood = np.exp(log_likes-log_likes.max())
-
-fout = open('like.txt', 'w')
-for i in range(n_values):
-    for j in range(n_values):
-        print(f_values[i], sig_values[j], likelihood[i, j], file=fout)
-fout.close()
-
-plotit=False
-if plotit:
-    if one_dim: 
-        plt.figure()
-        plt.plot(f_values, likelihood)
-        plt.axvline(f_expected, color='k', ls='--')
-        plt.xlabel('Growth-rate f')
-        plt.ylabel('Likelihood')
-    else:
-        from scipy.stats import chi2
-        #-- Cumulative distribution at 1, 2, 3 sigma for one degree of freedom
-        cdf = chi2.cdf([1, 4, 9], 1)
-        #-- corresponding chi2 values for 2 degrees of freedom
-        chi2_values = chi2.ppf(cdf, 2)
-        #-- corresponding likelihood values
-        like_contours = np.exp(-0.5*chi2_values)
-
-        plt.figure()
-        plt.pcolormesh(f_values, sig_values, likelihood.T, shading='nearest', cmap='gray_r')
-        plt.contour(f_values, sig_values, likelihood.T, levels=like_contours,
-                    colors='k', linestyles='--')
-        plt.colorbar()
-        plt.xlabel(r'Growth-rate $f$')
-        plt.ylabel(r'$\sigma_v$ [km/s]')
-
+        for j in range(n_values):
+            print(f_values[i], sig_values[j], likelihood[i, j], file=fout)
+    fout.close()
 
 
 #if __name__ == '__main__':
